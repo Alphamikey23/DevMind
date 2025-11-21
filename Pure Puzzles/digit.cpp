463b6a491bd2e85bcd84992ffb8463ac21523af0
AlphaMikey23 <eng22cse001@gmail.com>
2025-11-21 16:03:11 +0530

    Converted Digit to function

--- Pure Puzzles/digit.cpp
+++ Pure Puzzles/digit.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int digit;
-    cout<<"Enter a single digit number, 0-9: ";
-    cin>>digit;
+int doubleDigitValue(int digit) {
     int doubledDigit = digit * 2;
     int sum;
     if(doubledDigit >=10 ) sum = 1 + doubledDigit % 10;
     else
     sum = doubledDigit;
-    cout<<"Sum of digits in doubled number: "<<sum<<"\n";
+    return sum;
 }
