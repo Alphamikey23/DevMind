feeab7f596f09a14d136a993290654481cf16c9d
AlphaMikey23 <eng22cse001@gmail.com>
2025-12-08 12:58:07 +0530

    Added Padding

--- Pure Puzzles Challenges/shape_1.cpp
+++ Pure Puzzles Challenges/shape_1.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int main() {
     for (int i=1; i<=4; i++) {
+        
         for (int j=9; j>=abs(i * -2); j--) {
             cout<<"#";
         }
+        cout<<"         ";
         cout<<"\n";
     }
 }
---
+++ Pure Puzzles Challenges/spaces.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    for (int i=0;i<8;i++) {
+        cout<<i;
+        i = i + 1;
+    }
+    return 0;
+}
