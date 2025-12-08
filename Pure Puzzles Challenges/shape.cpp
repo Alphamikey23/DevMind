7c9073db4f009fe94172a1bf05be4c933c92b2ce
AlphaMikey23 <eng22cse001@gmail.com>
2025-12-08 12:24:03 +0530

    Added Exercises/Challenges

---
+++ Pure Puzzles Challenges/shape.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    for (int j =1 ; j<=4;j++) {
+        for (int i=1;i<=8;i++) {
+        cout<<"#";
+    }
+
+    
+    
+    cout<<"\n";
+}
+    
+    return 0;
+}
